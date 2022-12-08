aba51e40dde402842ed9c5deaa07f2e01c7f8fe9
Lavishq <v.whitehearted@simplelogin.co>
2022-12-08 21:17:45 +0530

    added code for 1 round

--- external.js
+++ external.js
@@ -1,4 +1,35 @@
 const getComputerChoice = () => {
   return ["Rock", "Paper", "Scissors"][Math.floor([Math.random() * 3])];
 };
-console.log(getComputerChoice());
+
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    return "Draw! Both same";
+  }
+  if (
+    playerSelection.toLowerCase() === "rock" &&
+    computerSelection === "Paper"
+  ) {
+    return "You Lose! Paper beats Rock";
+  }
+  if (
+    playerSelection.toLowerCase() === "paper" &&
+    computerSelection === "Scissors"
+  ) {
+    return "You Lose! Scissors beats Paper";
+  }
+  if (
+    playerSelection.toLowerCase() === "scissors" &&
+    computerSelection === "Rock"
+  ) {
+    return "You Lose! Rock beats Scissors";
+  } else {
+    return `You Win! ${
+      playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+    } beats ${computerSelection}`;
+  }
+};
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
